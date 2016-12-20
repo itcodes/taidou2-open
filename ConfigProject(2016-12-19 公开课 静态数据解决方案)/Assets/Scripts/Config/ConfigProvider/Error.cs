4934fe61d5aa171e3437a04d1c5ab828ea8d297d
zhangwen <izhangwen@outlook.com>
2016-12-20 15:15:39 +0800

    12月19日公开课源码

---
+++ ConfigProject(2016-12-19 公开课 静态数据解决方案)/Assets/Scripts/Config/ConfigProvider/Error.cs
@@ -0,0 +1,49 @@
+﻿
+namespace XHConfig
+{
+    /// <summary>
+    /// 错误提示的位置
+    /// </summary>
+
+
+    public partial class Error  : XmlConfig<Error>
+    {
+        
+        /// <summary>
+        /// 如果配置文件类名和文件名不同，重写Name
+        /// 如果相对路径不同，重写Path
+        /// </summary>
+        protected override void Init()
+        {
+            base.Init();
+            Info.Name = "ErrorConfig";
+        }
+        
+
+        public static E_ErrorLocal GetLocalById(int id)
+        {
+            ErrorCodeConfigContent error = System.Array.Find<ErrorCodeConfigContent>(Config.Content, delegate (ErrorCodeConfigContent item)
+            {
+                return item.Id == id;
+            });
+            if (null == error)
+            {
+                return E_ErrorLocal.None;
+            }
+            return (E_ErrorLocal)error.Local;
+        }
+
+        public static string GetErrorById(int id)
+        {
+            ErrorCodeConfigContent error = System.Array.Find<ErrorCodeConfigContent>(Config.Content, delegate (ErrorCodeConfigContent item)
+            {
+                return item.Id == id;
+            });
+            if (null == error)
+            {
+                return "配置表中没有找到该错误信息！Error Id：" + id;
+            }
+            return error.Desc;
+        }
+    }
+}
---
+++ ConfigProject(2016-12-19 公开课 静态数据解决方案)/Assets/Scripts/Config/ConfigProvider/ErrorCodeConfig.cs
@@ -0,0 +1,68 @@
+﻿
+namespace XHConfig
+{
+    /// <summary>
+    /// 错误提示的位置
+    /// </summary>
+    public enum E_ErrorLocal
+    {
+        None,
+        Fly,
+        MessageBox,
+    }
+
+    public partial class ErrorCodeConfig  : XmlConfig<ErrorCodeConfig>
+    {
+        /*
+        /// <summary>
+        /// 如果配置文件类名和文件名不同，重写Name
+        /// 如果相对路径不同，重写Path
+        /// </summary>
+        protected override void Init()
+        {
+            Path = "/Xml/";
+            Extension = ".xml";
+            Name = "ErrorCodeConfig";
+        }
+        */
+
+        public static E_ErrorLocal GetLocalById(int param)
+        {
+            //ErrorCodeConfigContent error = System.Array.Find<ErrorCodeConfigContent>(Config.Content, delegate (ErrorCodeConfigContent item)
+            //{
+            //    return item.Id == param;
+            //});
+            //if (null == error)
+            //{
+            //    return E_ErrorLocal.None;
+            //}
+            //return (E_ErrorLocal)error.Local;
+            var ret = System.Array.Find<ErrorCodeConfigContent>(Config.Content, (item) => item.Id == param);
+            if (ret != null)
+            {
+                return (E_ErrorLocal)ret.Local;
+            }
+            return E_ErrorLocal.None;
+        }
+
+        public static string GetErrorById(int param)
+        {
+            //ErrorCodeConfigContent error = System.Array.Find<ErrorCodeConfigContent>(Config.Content, delegate (ErrorCodeConfigContent item)
+            //{
+            //    return item.Id == param;
+            //});
+            //if (null == error)
+            //{
+            //    return "配置表中没有找到该错误信息！Error Id：" + param;
+            //}
+            //return error.Desc;
+
+            var ret = System.Array.Find<ErrorCodeConfigContent>(Config.Content, (item) => item.Id == param);
+            if (ret != null)
+            {
+                return ret.Desc;
+            }
+            return "Error: Not Find Error id:" + param;
+        }
+    }
+}
